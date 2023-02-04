9a77c90150fd82e433d30524ac62447fe0801acd
Ojo Oluwapelumi <oluwapelumiojo05@gmail.com>
2023-02-04 12:50:58 +0100

    hom bg image back

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 import Hero from '../Components/Hero'
 import Nav from '../Components/Nav'
-import '../Styles/Home.css'
+
+const StyledHome = styled.div`
+background-image: url(${require(`../assets/home/background-home-mobile.jpg`)});
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 120vh;
+  padding: 20px;
+
+@media (min-width: 576px) {
+  background-image: url(${require(`../assets/home/background-home-tablet.jpg`)});
+      padding: 20px 0px 20px 20px;
+}
+
+@media (min-width: 769px) {
+  background-image: url(${require(`../assets/home/background-home-desktop.jpg`)});
+      height: 93.3vh;
+      background-size:auto;
+}
+`
 
 const Home = () => {
   return (
-    <div className='home'>
+    <StyledHome>
       <Nav />
       <Hero />
-    </div>
+    </StyledHome>
   )
 }
 
